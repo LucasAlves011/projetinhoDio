bc7eca98d15bd43919a80f28cf5143a91fb50ea2
Lucas Alves <lucas.nascimento.al16@gail.com>
2022-01-05 11:23:52 -0300

    primeiro commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import com.*;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        System.out.println(LocalDate.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
+
+
+
+    }
+}
